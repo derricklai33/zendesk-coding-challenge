fcbb2a8a377a3f619bb04872ab5c600f0c43a3bd
derricklai33 <derricklai33@hotmail.com>
2021-05-11 14:55:47 +1000

    Tickets controller updated for getting all tickets dynamically back from the API call without using slice

--- controller/tickets_controller.js
+++ controller/tickets_controller.js
@@ -1,49 +1,50 @@
-const axios = require('axios');
-const { response } = require('express');
+const axios = require("axios");
+const { response } = require("express");
 
-function getAllTickets(req, res) {
-  let url = `https://derricklai-zendesk-challenge.zendesk.com/api/v2/tickets.json`
-  try{
-    axios.get(url, {
-      auth: {
-        username: 'derrick.laijy@gmail.com',
-        password: 'q1w2e3r4t5',
-      }
-    })
-      .then(response => response.data)
-      .then(data => {
-        let dataObj = {
-          chunk1: data.tickets.slice(0, 25),
-          chunk2: data.tickets.slice(26, 50),
-          chunk3: data.tickets.slice(51, 75),
-          chunk4: data.tickets.slice(76, 100)
-        }
-        return dataObj;
-      })
-      .then(obj => res.send(obj));
-  } catch(error) {
+async function getAllTickets(req, res) {
+  let tickets = [];
+  let url = `https://derricklai-zendesk-challenge.zendesk.com/api/v2/tickets.json`;
+  try {
+    do {
+      const responseData = await axios
+        .get(url, {
+          auth: {
+            username: "derrick.laijy@gmail.com",
+            password: "q1w2e3r4t5",
+          },
+        })
+        .catch((error) => {
+          res.send(error);
+          url = null;
+        });
+      tickets = [...tickets, ...responseData.data.tickets];
+      url = responseData.data.next_page;
+    } while (url !== null);
+    res.send(tickets);
+  } catch (error) {
     console.error(error);
   }
 }
 
 function getSpecificTickets(req, res) {
-  let url = `https://derricklai-zendesk-challenge.zendesk.com/api/v2/tickets/${req.params.ticket_id}.json`
-  try{
-    axios.get(url, {
-      auth: {
-        username: 'derrick.laijy@gmail.com',
-        password: 'q1w2e3r4t5',
-      }
-    })
-      .then(response => response.data)
-      .then(data => res.send(data))
-      .catch(error => res.send(error))
-  } catch(error) {
+  let url = `https://derricklai-zendesk-challenge.zendesk.com/api/v2/tickets/${req.params.ticket_id}.json`;
+  try {
+    axios
+      .get(url, {
+        auth: {
+          username: "derrick.laijy@gmail.com",
+          password: "q1w2e3r4t5",
+        },
+      })
+      .then((response) => response.data)
+      .then((data) => res.send(data))
+      .catch((error) => res.send(error));
+  } catch (error) {
     console.log(error);
   }
 }
 
 module.exports = {
   getAllTickets,
-  getSpecificTickets
-} 
+  getSpecificTickets,
+};
