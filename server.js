9deb2d824080b7478f5e27d58b6461f8779741fb
derricklai33 <derricklai33@hotmail.com>
2021-05-13 12:26:09 +1000

    Added in dotenv require in server.js, as it is recommended to be placed where the application is first called

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const routes = require('./routes/routes');
 const PORT = process.env.PORT || 8080;
 const app = express();
+require('dotenv').config();
 
 app.use('/tickets', routes);
 
