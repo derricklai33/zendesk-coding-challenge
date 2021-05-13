59c529c53f29dbebb85261c0cc950d02af427d36
derricklai33 <derricklai33@hotmail.com>
2021-05-13 13:54:14 +1000

    Updated controller to return error messages accordingly. Used best practice of try catct finally instead of just try catch block

--- controller/tickets_controller.js
+++ controller/tickets_controller.js
@@ -1,7 +1,8 @@
 const axios = require("axios");
+const { response } = require("express");
 
 async function getAllTickets(_req, res) {
-  let tickets = [];
+  let data = {tickets: [], error: ""};
   let url = `https://derricklai-zendesk-challenge.zendesk.com/api/v2/tickets.json`;
   try {
     do {
@@ -12,34 +13,52 @@ async function getAllTickets(_req, res) {
             password: process.env.PASSWORD,
           },
         })
-        .catch((error) => {
-          res.send(error);
-          url = null;
-        });
-      tickets = [...tickets, ...responseData.data.tickets];
+      data.tickets = [...data.tickets, ...responseData.data.tickets];
       url = responseData.data.next_page;
     } while (url !== null);
-    res.send(tickets);
   } catch (error) {
-    console.error(error);
+    console.log(error.message);
+    data.error = errorMessages(error.response.status);
+  } finally {
+    res.send(data);
   }
 }
 
-function getSpecificTickets(req, res) {
+async function getSpecificTickets(req, res) {
+  let data = {ticket: {}, error: ""};
   let url = `https://derricklai-zendesk-challenge.zendesk.com/api/v2/tickets/${req.params.ticket_id}.json`;
   try {
-    axios
+    const responseData = await axios
       .get(url, {
         auth: {
           username: process.env.LOGIN,
           password: process.env.PASSWORD,
         },
       })
-      .then((response) => response.data)
-      .then((data) => res.send(data))
-      .catch((error) => res.send(error));
+    data.ticket = responseData.data.ticket;
   } catch (error) {
-    console.log(error);
+    console.log(error.message)
+    data.error = errorMessages(error.response.status);
+  } finally {
+    res.send(data);
+  }
+}
+
+function errorMessages(statusCode) {
+  let message;
+  switch(statusCode) {
+    case 400:
+      message = "Error 400, id is not valid";
+      return message;
+    case 401:
+      message = "Error 401, could not authenticate you";
+      return message;
+    case 404:
+      message = "Error 404, id does not exist";
+      return message;
+    default:
+      message = "Unknown error, contact website admin"
+      return message;
   }
 }
 
