ec6ebb9ce1debb87d565c3809936f026a161649a
derricklai33 <derricklai33@hotmail.com>
2021-05-15 16:53:46 +1000

    Updated TicketViewer component to display error messages or data accordingly.

--- client/src/components/TicketViewer.jsx
+++ client/src/components/TicketViewer.jsx
@@ -1,32 +1,37 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 export function TicketViewer(props) {
-  const [ data, setData ] = useState();
+  const [data, setData] = useState();
 
-  useEffect (() => {
+  useEffect(() => {
     fetchSpecificTicket(props.match.params.id)
-  }, [])
-
+  }, [props.match.params.id])
   async function fetchSpecificTicket(id) {
-    try{
-      const response = await axios.get(`${process.env.REACT_APP_NODE_API_URL}/tickets/${id}`);
-      console.log(response.data)
+    try {
+      const response = await axios.get(
+        `${process.env.REACT_APP_NODE_API_URL}/tickets/${id}`
+      );
       setData(response.data);
     } catch (error) {
       console.log(error);
     }
   }
 
-  return(
+  return (
     <>
-      <h1>Ticket number {props.match.params.id}</h1>
-      {data && (
-      <>
-        <h1>{data.ticket.subject}</h1>
-        <p>{data.ticket.description}</p>
-      </>
-      )}
+      <h1>Ticket #{props.match.params.id}</h1>
+      <Link to="/">{`< Back`}</Link>
+      {data &&
+        (data.error === "" ? (
+          <>
+            <h1>{data.ticket.subject}</h1>
+            <p>{data.ticket.description}</p>
+          </>
+        ) : (
+        <h2>{data.error}</h2>
+        ))}
     </>
-  )
+  );
 }
