65165ad3e93ebe052d7bb9f7c1449f5c6ec07f79
derricklai33 <derricklai33@hotmail.com>
2021-05-15 22:53:51 +1000

    Added a page display element and styled All Tickets page

--- client/src/components/AllTicketsViewer.jsx
+++ client/src/components/AllTicketsViewer.jsx
@@ -8,24 +8,27 @@ export function AllTicketViewer(props) {
   return (
     <>
       <h1>All Tickets</h1>
+      <div className={"ticket-viewer-div"}>
       {data && (
         data.tickets.length > 0 ? (
           data.tickets[pages].map((item, index) => {
-          return(
-            <Link key={index} to={`/${item.id}`}>
-              {`${item.id}. ${item.subject}`}
+            return(
+              <Link key={index} to={`/${item.id}`}>
+              {`${item.id}. ${item.subject}`} <br/>
             </Link>
           )
-          })
+        })
         ) :
         <h2>{data.error}</h2>
-      )}
-      {data && (
+        )}
+      {data && data.tickets.length > 0 &&(
         <>
         {pages !== 0 ? <button onClick={() => { setPages(pages - 1)}}> {`<`} </button>: <button disabled={true}> {'<'}</button>}
+        {pages + 1}
         {pages === data.tickets.length - 1 ? <button disabled={true}>{'>'}</button> : <button onClick={() => { setPages(pages + 1)}}> {`>`} </button>}
         </>
       )}
+      </div>
     </>
   );
 }
