1390014d3e4d4cf523cdfc98788b8034468de2c2
derricklai33 <derricklai33@hotmail.com>
2021-05-15 16:50:54 +1000

    Minor comments added

--- routes/routes.js
+++ routes/routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { getAllTickets, getSpecificTickets} = require('../controller/tickets_controller');
 
+// routes for controller
 router.get('/:ticket_id', getSpecificTickets)
 router.get('/', getAllTickets)
 
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const app = express();
 require('dotenv').config();
 const cors = require('cors');
 
+// cors for communicating with react app
 app.use(cors())
 
 app.use('/tickets', routes);
